8907ae5073caaa488a0865fafa9f6043432b4acc
Zachary Townsend <zach@verbbrands.com>
2020-11-15 18:56:15 +0100

    chore(Tailwind): add spacings

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
   },
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '7\.5': '1.875rem',
+        '9': '2.25rem',
+        '18': '4.5rem',
+        '72': '18rem'
+      }
+    },
   },
   variants: {},
   plugins: [],
