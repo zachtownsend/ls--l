e481a1a24c4bc269cc6f23d2c61acbeb4e1f36f3
Zachary Townsend <zach@verbbrands.com>
2020-11-17 23:07:41 +0100

    feat(BlockCards): add component

--- src/components/BlockCards.js
+++ src/components/BlockCards.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import ArrowLink from './ArrowLink';
+import BlockTitle from './BlockTitle';
+import Card from './Card';
 
-const BlockCards = () => {
+const BlockCards = ({ settings }) => {
+    const { bgcolor, blocktitleenabled, blocktitle, cards, customBgcolor } = settings;
+    console.log({ cards });
     return (
-        <div>
-            <h2>This is a block card componenet!</h2>
+        <div className={`bg-${bgcolor} pb-20 pt-10.5 lg:pt-15`} style={bgcolor === 'custom' && {background: customBgcolor}}>
+            {blocktitleenabled && <BlockTitle pretitle={blocktitle.pretitle} title={blocktitle.title} posttitle={blocktitle.posttitle} />}
+            <div className="container">
+                <ul className="grid gap-4 grid-cols-1 lg:grid-cols-3">
+                    {cards.map(card =>
+                        <li className="flex justify-center">
+                            <Card
+                                image={<img src={card.image.localFile.publicURL} />}
+                                title={card.cardtitle}
+                                content={card.cardcontent}
+                                footer={<ArrowLink className="font-bold" href={card.link.url} target={card.link.target} title={card.link.title || 'Learn More'} />}
+                            />
+                        </li>)
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
