3dc6e2b7a5932bb76192d05714d591bf4137d67a
Zachary Townsend <zach@verbbrands.com>
2020-11-18 23:25:16 +0100

    refactor(Blocks): fix warnings in components

--- src/components/ArrowLink.js
+++ src/components/ArrowLink.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ArrowLink = ({ className, href, target, title }) => {
     const arrow = (
-        <svg width="6" height="11"><path d="M.068 9.923L1.044 11 6 5.53.989 0 0 1.09l4.036 4.455z" fill="#000" fill-rule="nonzero"/></svg>
+        <svg width="6" height="11"><path d="M.068 9.923L1.044 11 6 5.53.989 0 0 1.09l4.036 4.455z" fill="#000" fillRule="nonzero"/></svg>
     )
     return (
         <a className={className} href={href} target={target}>
